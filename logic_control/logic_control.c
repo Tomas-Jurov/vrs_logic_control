d5549ce14325340af760f8fdc9c34a4987785d4f
Martin Lukáč <xlukacm3@stuba.sk>
2022-12-17 11:04:55 +0100

    please test joyStickPositionConverter

--- logic_control/logic_control.c
+++ logic_control/logic_control.c
@@ -12,6 +12,16 @@ int8_t fb = 0;
 int8_t ud = 0;
 int8_t yv = 0;
 int8_t cm = 4;
+
+int8_t deadzone= 20;
+int8_t x_speed = 0;
+int8_t y_speed = 0;
+int8_t z_speed = 0;
+int8_t yaw_speed = 0;
+
+int8_t oneMin = 0;
+int8_t oneMax = 0;
+
 uint32_t RC_Commands[4];
 bool not_in_air = true;
 bool rc_control_on = false;
@@ -28,10 +38,56 @@ bool funcb(){
 }
 
 void get_and_send_data(void){
+	//min max , data na mapovanie
+
+	//toto neviem co s tym chces robit
 	lr = map(*RC_Commands,806,4032,-100,100);
 	fb = map(*(RC_Commands+1),347,3400,-100,100);
 	ud = map(*(RC_Commands+2),3155,840,-100,100);
 	yv = map(*(RC_Commands+3),3117,467,-100,100);
+
+
+	if((map((*RC_Commands),(*RC_Commands),4032,0,100)<=(map((*RC_Commands),(*RC_Commands)+deadzone,4032,0,100)))&&(map((*RC_Commands),(*RC_Commands),829,0,-100)>=(map((*RC_Commands),(*RC_Commands)-deadzone,829,0,-100)))){
+		x_speed = 0;
+	}
+	if((map((*RC_Commands),(*RC_Commands),4032,0,100))>(map((*RC_Commands),(*RC_Commands)+deadzone,4032,0,100))){
+		x_speed = map((*RC_Commands),(*RC_Commands),4032,0,100);
+	}
+	if((map((*RC_Commands),(*RC_Commands),829,0,-100))<(map((*RC_Commands),(*RC_Commands)-deadzone,829,0,-100))){
+		x_speed = (map((*RC_Commands),(*RC_Commands),829,0,-100));
+	}
+	///////////////////////////////////////////////////////
+	if((map((*RC_Commands+1),(*RC_Commands+1),3400,0,100)<=(map((*RC_Commands+1),(*RC_Commands+1)+deadzone,3400,0,100)))&&(map((*RC_Commands+1),(*RC_Commands+1),347,0,-100)>=(map((*RC_Commands+1),(*RC_Commands+1)-deadzone,347,0,-100)))){
+		y_speed = 0;
+	}
+	if((map((*RC_Commands+1),(*RC_Commands+1),3400,0,100))>(map((*RC_Commands+1),(*RC_Commands+1)+deadzone,3400,0,100))){
+		y_speed = map((*RC_Commands+1),(*RC_Commands+1),3400,0,100);
+	}
+	if((map((*RC_Commands+1),(*RC_Commands+1),347,0,-100))<(map((*RC_Commands+1),(*RC_Commands+1)-deadzone,347,0,-100))){
+		y_speed = (map((*RC_Commands+1),(*RC_Commands+1),347,0,-100));
+	}
+
+	/////////////////////////////////////////////////////////////////
+	if((map((*RC_Commands+2),(*RC_Commands+2),3155,0,100)<=(map((*RC_Commands+2),(*RC_Commands+2)+deadzone,3155,0,100)))&&(map((*RC_Commands+2),(*RC_Commands+2),840,0,-100)>=(map((*RC_Commands+2),(*RC_Commands+2)-deadzone,840,0,-100)))){
+		z_speed = 0;
+	}
+	if((map((*RC_Commands+2),(*RC_Commands+2),3155,0,100))>(map((*RC_Commands+2),(*RC_Commands+2)+deadzone,3155,0,100))){
+		z_speed = map((*RC_Commands+2),(*RC_Commands+2),3155,0,100);
+	}
+	if((map((*RC_Commands+2),(*RC_Commands+2),840,0,-100))<(map((*RC_Commands+2),(*RC_Commands+2)-deadzone,840,0,-100))){
+		z_speed = (map((*RC_Commands+2),(*RC_Commands+2),840,0,-100));
+	}
+	///////////////////////////////////////////////////
+	if((map((*RC_Commands+3),(*RC_Commands+3),3117,0,100)<=(map((*RC_Commands+3),(*RC_Commands+3)+deadzone,3117,0,100)))&&(map((*RC_Commands+3),(*RC_Commands+3),467,0,-100)>=(map((*RC_Commands+3),(*RC_Commands+3)-deadzone,467,0,-100)))){
+		yaw_speed = 0;
+	}
+	if((map((*RC_Commands+3),(*RC_Commands+3),3117,0,100))>(map((*RC_Commands+3),(*RC_Commands+3)+deadzone,3117,0,100))){
+		yaw_speed = map((*RC_Commands+3),(*RC_Commands+3),3117,0,100);
+	}
+	if((map((*RC_Commands+3),(*RC_Commands+3),467,0,-100))<(map((*RC_Commands+3),(*RC_Commands+3)-deadzone,467,0,-100))){
+		yaw_speed = (map((*RC_Commands+3),(*RC_Commands+3),467,0,-100));
+	}
+
 
     if(lr<-80 && fb<-90 && ud<-90 && yv>90 && (not_in_air==true)){
     	bool success = funcb();
